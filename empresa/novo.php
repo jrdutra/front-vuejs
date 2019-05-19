d4e94c5573f8e9838908c27266a67555759b5ea6
jrdutra <jrdutra.com.br@gmail.com>
2019-05-19 11:46:22 -0300

    Inclusão de empresas concluidos

--- empresa/novo.php
+++ empresa/novo.php
@@ -1,40 +1,77 @@
-<div id="appNovoFornecedor">
+<div id="appNovaEmpresa">
     <div class="container">
         <div class="card mt-4">
             <div class="card-header">
-                Incluir novo fornecedor
+                Incluir nova empresa
             </div>
             <div class="row mt-4">
                 <div class="col">
                     <div class="container">
-                        <!-- INPUT NOME-->
+                        <!-- INPUT Fantasia-->
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
-                                <span class="input-group-text" id="idlblnome">Nome</span>
+                                <span class="input-group-text" id="idlblnome">Nome Fantasia</span>
                             </div>
-                                <input v-model="fornecedorAIncluir.nome" type="text" class="form-control" id="idtxtNome" aria-describedby="basic-addon3">
+                                <input v-model="empresaAIncluir.nomeFantasia" type="text" class="form-control" id="idtxtNomeFantasia" aria-describedby="basic-addon3">
                         </div>
+
+                        <!-- INPUT Razao Social-->
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="idlblrazaoSocial">Razão Social</span>
+                            </div>
+                                <input v-model="empresaAIncluir.razaoSocial" type="text" class="form-control" id="idtxtRazaoSocial" aria-describedby="basic-addon3">
+                        </div>
+
                         <!-- INPUT CNPJ-->
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
-                                <span class="input-group-text" id="idlblCnpj">CNPJ</span>
+                                <span class="input-group-text" id="idlblcnpj">Cnpj</span>
                             </div>
-                                <input v-model="fornecedorAIncluir.cnpj" type="text" class="form-control" id="idtxtCnpj" aria-describedby="basic-addon3">
+                                <input v-model="empresaAIncluir.cnpj" type="text" class="form-control" id="idtxtCnpj" aria-describedby="basic-addon3">
                         </div>
-                        <!-- INPUT Telefone-->
+
+                        <!-- INPUT Logradouro-->
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
-                                <span class="input-group-text" id="idlblTelefone">Telefone</span>
+                                <span class="input-group-text" id="idlbllogradouro">Logradouro</span>
                             </div>
-                                <input  v-model="fornecedorAIncluir.telefone" type="text" class="form-control" id="idtxtTelefone" aria-describedby="basic-addon3">
+                                <input v-model="empresaAIncluir.logradouro" type="text" class="form-control" id="idtxtLogradouro" aria-describedby="basic-addon3">
                         </div>
-                        <!-- INPUT Endereço-->
+
+                        <!-- INPUT Bairro-->
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
-                                <span class="input-group-text" id="idlblEndereco">Endereço</span>
+                                <span class="input-group-text" id="idlblBairro">Bairro</span>
                             </div>
-                                <input  v-model="fornecedorAIncluir.endereco" type="text" class="form-control" id="idtxtEndereco" aria-describedby="basic-addon3">
+                                <input v-model="empresaAIncluir.bairro" type="text" class="form-control" id="idtxtbairro" aria-describedby="basic-addon3">
                         </div>
+
+                        <!-- INPUT Cidade-->
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="idlblCidade">Cidade</span>
+                            </div>
+                                <input v-model="empresaAIncluir.cidade" type="text" class="form-control" id="idtxtcidade" aria-describedby="basic-addon3">
+                        </div>
+
+                        <!-- INPUT Estado-->
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="idlblEstado">Estado</span>
+                            </div>
+                                <input v-model="empresaAIncluir.estado" type="text" class="form-control" id="idtxtestado" aria-describedby="basic-addon3">
+                        </div>
+
+                        <!-- INPUT Email-->
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="idlblEmail">Email</span>
+                            </div>
+                                <input v-model="empresaAIncluir.email" type="text" class="form-control" id="idtxtemail" aria-describedby="basic-addon3">
+                        </div>
+                        
+                        
                         <div class="input-group mb-3">
                             <div v-for="erro in erros" class="row">
                                 <div class="col">
@@ -62,7 +99,7 @@
                             </div>
                         </div>
                         <div class="input-group mb-3 justify-content-end">
-                            <button v-on:click="incluirNovoFornecedor()" type="button" class="btn btn-success" data-dismiss="modal">Salvar</button>
+                            <button v-on:click="incluirNovaEmpresa()" type="button" class="btn btn-success" data-dismiss="modal">Salvar</button>
                         </div>
                     </div>
                 </div>
@@ -74,14 +111,13 @@
 <script>
     var mixin = {
         methods: {
-            incluirNovoFornecedor(){
-                console.log(this.fornecedorAIncluir);
+            incluirNovaEmpresa(){
                 var vm = this;
-                var url = 'http://' + host + ':8089/api/fornecedor/cadastrar';
-                this.fornecedorAIncluir.id = '';
-                axios.post(url, this.fornecedorAIncluir).then(function(r){
+                var url = 'http://' + host + ':8089/api/empresa/cadastrar';
+                this.empresaAIncluir.id = '';
+                axios.post(url, this.empresaAIncluir).then(function(r){
                     vm.erros = [];
-                    vm.sucessos = ["Fornecedor incluido com sucesso!"];
+                    vm.sucessos = ["Empresa incluida com sucesso!"];
                 }).catch(function (error) {
                         vm.erros = error.response.data.errors;
                         vm.sucessos = [];
@@ -91,13 +127,13 @@
             }
         }
     }
-    var appNovoFornecedor = new Vue({
-        el: "#appNovoFornecedor",
+    var appNovaEmpresa = new Vue({
+        el: "#appNovaEmpresa",
         mixins: [mixin],
         data: {
           erros:[],
           sucessos:[],
-          fornecedorAIncluir:{},
+          empresaAIncluir:{},
         }
     });
 </script>
